528a1f1c076ee39f63522d77d642c4af48f5a4f7
ParkAsher <devasherpark@naver.com>
2024-07-30 17:27:31 +0900

    [feat]: 글 등록 기능에 권한 guard 적용

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dtos/create-article.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('article')
 export class ArticleController {
     constructor(private readonly articleService: ArticleService) {}
 
     // 글 등록
+    @UseGuards(JwtAuthGuard)
     @Post('/')
     async createArticle(@Body() data: CreateArticleDto) {
         return await this.articleService.createArticle(data);
