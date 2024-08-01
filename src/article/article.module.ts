70da8a2ec18f1441d2afb5aeaa463319f71ddd61
ParkAsher <devasherpark@naver.com>
2024-08-01 16:14:08 +0900

    [feat]: 글 작성 시 해당 카테고리 글 수 증가 기능 구현

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -3,9 +3,10 @@ import { ArticleController } from './article.controller';
 import { ArticleService } from './article.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Article } from 'src/entities/article.entity';
+import { Category } from 'src/entities/category.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Article])],
+    imports: [TypeOrmModule.forFeature([Article, Category])],
     controllers: [ArticleController],
     providers: [ArticleService],
 })
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,17 +1,54 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Article } from 'src/entities/article.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { CreateArticleDto } from './dtos/create-article.dto';
+import { Category } from 'src/entities/category.entity';
 
 @Injectable()
 export class ArticleService {
     constructor(
         @InjectRepository(Article)
         private articleRepository: Repository<Article>,
+        @InjectRepository(Category)
+        private categoryRepository: Repository<Category>,
+        private dataSource: DataSource,
     ) {}
 
     async createArticle(data: CreateArticleDto) {
-        return await this.articleRepository.save(data);
+        const { title, content, categoryId, userId } = data;
+
+        const queryRunner = this.dataSource.createQueryRunner();
+
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+
+        try {
+            /* 비즈니스 로직 */
+            // 글 저장
+            await this.articleRepository.save({
+                title,
+                content,
+                categoryId,
+                userId,
+            });
+
+            // 카테고리 글 수 + 1
+            await this.categoryRepository.increment(
+                { id: categoryId },
+                'contentsCount',
+                1,
+            );
+
+            // 커밋
+            await queryRunner.commitTransaction();
+        } catch (error) {
+            // 트랜잭션 실패 시 롤백.
+            console.log(error);
+            await queryRunner.rollbackTransaction();
+        } finally {
+            // 트랜잭션 종료 시 연결 종료.
+            await queryRunner.release();
+        }
     }
 }
